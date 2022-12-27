313e8c234064af71c1fab1794ff6c7d0b01a2d85
Mohamed Kiouaz <mohamed.kiouaz@gmail.com>
2022-12-27 10:35:21 +0100

    Updated readability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import moviepy.editor as mp
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import imageio
 import fastai
@@ -10,10 +9,17 @@ from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 
 
-folder = r"D:\Videos\Radeon ReLive\Apex Legends\a"
+def expand_ones(arr, N, M):
+    """
+    Expand the ones in an array of zeros and ones.
+    
+    Given an array `arr` containing zeros and ones, this function expands the ones by replacing
+    the elements before and after each one with ones as well. The number of elements to replace
+    before and after each one is given by the parameters `N` and `M`, respectively.
+    
+    """
+    arr = np.array(arr)
 
-
-def expand_zeros(arr, N, M):
     # Find the indices of the zeros in the array
     N = int(N)
     M = int(M)
@@ -28,40 +34,61 @@ def expand_zeros(arr, N, M):
     print(arr.sum() / 44100)
     return arr
 
-def get_frame(image, position, frame_size):
+def get_subframe(image, position, subframe_size):
+    """
+    Given an input image and a position, extract a subframe of size subframe_size
+    """
     rows, cols, _ = image.shape
 
-    frame_rows, frame_cols = frame_size
+    frame_rows, frame_cols = subframe_size
 
-    # Calculate top-left corner of frame
     row_min = position[0] - frame_rows // 2
     col_min = position[1] - frame_cols // 2
 
-    if row_min < 0:
-        row_min = 0
-    if col_min < 0:
-        col_min = 0
+    row_min = max(row_min, 0)
+    col_min = max(col_min, 0)
     if row_min + frame_rows > rows:
         row_min = rows - frame_rows
     if col_min + frame_cols > cols:
         col_min = cols - frame_cols
 
-    frame = image[row_min:row_min+frame_rows, col_min:col_min+frame_cols]
+    return image[row_min:row_min+frame_rows, col_min:col_min+frame_cols]
 
-    return frame
+def create_subclip(clip, indices):
+    """
+    Given a clip and the indices of the relavant frames, create a clip only with those frames
+    """
+    subclips = []
 
+    start = 0
+    for i in range(1, len(indices)):
+        if indices[i] != indices[i-1] + 1:
+            # If the current index is not consecutive with the previous index, create a new subclip
+            subclips.append(clip.subclip(t_start=indices[start] / clip.fps, t_end=indices[i-1] / clip.fps))
+            start = i
+
+    subclips.append(clip.subclip(t_start=indices[start] / clip.fps, t_end=indices[-1] / clip.fps))
+    
+    subclip = mp.concatenate_videoclips(subclips)
+
+    return subclip
+
+folder = r"D:\Videos\Radeon ReLive\Apex Legends\a"
 generate_inputs = False
-
-predict_sampling = 25
+input_generation_sampling = 50 # if in input generation, generate one input every 
+predict_sampling = 25 # if in prediction mode, use one frame every
+keep_after = 5 # sec
+keep_before = 4 # sec
 
 for filename in os.listdir(folder):
     if filename.endswith(".mp4"):
         path = os.path.join(folder, filename)
-        print(path)
+        print(f'Working on {path}.')
         clip = mp.VideoFileClip(path)
         audio = mp.AudioFileClip(path)
 
         if not generate_inputs:
+            # We need to load the model to make prediction if a frame is intresting or not
             path = Path('inputs')
             data = ImageDataLoaders.from_folder(path, train='.', valid_pct=0.2)
             input_shape = data.one_batch()[0].shape[1:]
@@ -72,15 +99,16 @@ for filename in os.listdir(folder):
         i = 0
         for frame in tqdm(clip.iter_frames(), total=int(clip.duration * clip.fps)):
             i += 1
-            if generate_inputs and i % 50 != 0:
+            if generate_inputs and i % input_generation_sampling != 0:
                 continue
             
             if not generate_inputs and i % predict_sampling != 0:
                 mask.append(False)
                 continue
 
-            subf = {'center': get_frame(frame, (719, 1289), (100, 100)),
-                    'teamsleft': get_frame(frame, (88, 2049), (100, 100))}
+            subf = {'center': get_subframe(frame, (719, 1289), (100, 100)),
+                    'teamsleft': get_subframe(frame, (88, 2049), (100, 100))}
+
             pred = []
             for key, subframe in subf.items():
                 subframe = subframe.astype(np.uint8)
@@ -94,23 +122,11 @@ for filename in os.listdir(folder):
             mask.append(any(pred))
 
         if not generate_inputs:
-            mask = np.array(mask)
-
-            mask = expand_zeros(mask, 4 * clip.fps, 1 * clip.fps)
+            mask = expand_ones(mask, keep_before * clip.fps, keep_after * clip.fps)
 
             mask_indices = mask.nonzero()[0]
-
-            subclips = []
-
-            start = 0
-            for i in range(1, len(mask_indices)):
-                if mask_indices[i] != mask_indices[i-1] + 1:
-                    # If the current index is not consecutive with the previous index, create a new subclip
-                    subclips.append(clip.subclip(t_start=mask_indices[start] / clip.fps, t_end=mask_indices[i-1] / clip.fps))
-                    start = i
-
-            subclips.append(clip.subclip(t_start=mask_indices[start] / clip.fps, t_end=mask_indices[-1] / clip.fps))
             
-            filtered_clip = mp.concatenate_videoclips(subclips)
-
+            filtered_clip = create_subclip(clip, mask_indices)
+            
             filtered_clip.write_videofile(f"videos/{filename[:-4]}_shortened.mp4")
+
--- ml.py
+++ ml.py
@@ -3,27 +3,30 @@ import fastai.vision as vision
 from pathlib import Path
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
-
+from fastai.metrics import accuracy
 
 
 if __name__ == '__main__':
-    # Set the batch size and the number of epochs
-    bs = 64
     epochs = 30
 
     path = Path('inputs')
 
     data = ImageDataLoaders.from_folder(path, train='.', valid_pct=0.05)
+    print(f'training set: {data.train_ds}')
+    print(f'validation set: {len(data.valid_ds)}')
 
     input_shape = data.one_batch()[0].shape[1:]
-    learn = vision_learner(data, vision.models.resnet50, bn_final=True, model_dir="models")
+    learn = vision_learner(data, vision.models.resnet50, bn_final=True, model_dir="models", metrics=[accuracy])
     
+
+    learn = vision_learner(data, vision.models.resnet50, bn_final=True, model_dir="models")
+
     try:
         learn.load('model')
-    except:
+    except Exception:
         print("Cannot load model, starting from scratch.")
 
-    for i in range(epochs   ):
+    for _ in range(epochs):
         learn.fit(1)
 
         learn.save('model')
