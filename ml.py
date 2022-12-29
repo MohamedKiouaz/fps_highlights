69f0d618ab93fac23ee24ca1fc0884ebfb7adc1e
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2022-12-29 22:32:15 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 from fastai.metrics import accuracy
+from fastai.vision.models import resnet18
 
 
 if __name__ == '__main__':
@@ -12,13 +13,16 @@ if __name__ == '__main__':
     path = Path('inputs')
 
     data = ImageDataLoaders.from_folder(path, train='.', valid_pct=0.05)
-    print(f'training set: {data.train_ds}')
+    print(f'training set: {len(data.train_ds)}')
     print(f'validation set: {len(data.valid_ds)}')
 
     input_shape = data.one_batch()[0].shape[1:]
-    learn = vision_learner(data, vision.models.resnet50, bn_final=True, model_dir="models", metrics=[accuracy])
+    learn = vision_learner(data, resnet18, bn_final=True, model_dir="models", metrics=[accuracy])
+    print(learn.summary())
     
-    for _ in range(epochs):
-        learn.fit(1)
+    #learn.load('model')
 
-        learn.save('model')
+    for _ in range(epochs):
+        print(learn.fit(1))
+
+        learn.save('resnet18')
