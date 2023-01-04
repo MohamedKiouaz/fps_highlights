f7c2751e91ec39b5a2886e989a72484e06b42a47
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-04 17:36:11 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -6,40 +6,70 @@ from fastai.metrics import accuracy
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 from fastai.vision.models import resnet18
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+
+
+def plot_roc_curve(y_test, y, path):
+    # Compute ROC curve and ROC area for each class
+    fpr, tpr, _ = roc_curve(y_test, y)
+    roc_auc = auc(fpr, tpr)
+
+    # Plot of a ROC curve for a specific class
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label=f'ROC curve (area = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig(path)
+
+def plot_n_images(images, pred, true, path):
+    # Display the most uncertain images
+    n_images = len(images)
+    n_rows = int(np.sqrt(n_images))
+    n_cols = int(np.sqrt(n_images))
+
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(10, 10))
+
+    for i, ax in enumerate(axs.flat):
+        if i > len(axs.flat):
+            break
+        ax.imshow(images[i])
+        ax.set_title(f'Pred:{pred[i]:.2f} GT:{true[i]:.2f}')
+        ax.axis('off')
+
+    fig.tight_layout()
+    
+    fig.savefig(path)
+
 
 if __name__ == '__main__':
-    epochs = 30
-
     path = Path('inputs')
-
-    data = ImageDataLoaders.from_folder(path, train='.', valid_pct=0.05)
+    data = ImageDataLoaders.from_folder(path, train='train', valid='valid')
 
     learn = vision_learner(data, resnet18, bn_final=True, model_dir="models", metrics=[accuracy])
     learn.load("resnet18")
+
+    save_path = Path('graphs')
+    save_path.mkdir(exist_ok=True)
+
+    y_test = np.array([img[1].numpy() for img in data.valid_ds])
+    y = np.array([learn.predict(img[0])[2][1].numpy() for img in data.valid_ds])
+    imgs = [np.asarray(img[0]) for img in data.valid_ds]
     
-    predictions = []
-    uncertainties = []
-    for img in data.train_ds:
-        prediction = learn.predict(img[0])[2].numpy()
-        predictions.append(prediction)
-        uncertainty = np.max(prediction)
-        uncertainties.append(uncertainty)
-    
-    sorted_idxs = np.argsort(uncertainties)
-    sorted_images = [data.train_ds[i] for i in sorted_idxs]
-    sorted_predictions = [predictions[i] for i in sorted_idxs]
+    # Plot ROC curve
+    plot_roc_curve(y_test, y, save_path / 'roc.png')
 
-    # Display the most uncertain images
-    n_images = 25
-    fig, axs = plt.subplots(5, 5)
-    for i, ax in enumerate(axs.flat):
-        # how to show a pilimage
-        ax.imshow(np.asarray(sorted_images[i][0]))
-        # add title
-        ax.set_title(f'{sorted_predictions[i][0]:.2f} {sorted_predictions[i][1]:.2f}')
-        ax.axis('off')
+    # Plot most uncertain images
+    certainties = 1 - abs(0.5 - y)
+    sorted_idxs = np.argsort(certainties)[:25]
+    sorted_images = [imgs[i] for i in sorted_idxs]
+    sorted_predictions = [certainties[i] for i in sorted_idxs]
+    sorted_y_test = [y_test[i] for i in sorted_idxs]
 
-    # tight layout
-    plt.tight_layout()
-    plt.show()
-
+    plot_n_images(sorted_images, sorted_predictions, sorted_y_test, save_path / 'most_uncertain.png')
