b41e235e05a8fc33796ef6430e9c87a8a9bfe56d
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-06 22:53:59 +0100

    Moved functions from main

--- main.py
+++ main.py
@@ -1,83 +1,26 @@
-import moviepy.editor as mp
 import os
-import numpy as np
-from tqdm import tqdm
-import imageio
-from fastai.vision.all import *
-from fastai.vision.data import ImageDataLoaders
-from fastai.vision.learner import vision_learner
+from concurrent.futures import ThreadPoolExecutor
 
-def expand_ones(arr, N, M):
-    """
-    Expand the ones in an array of zeros and ones.
-    
-    Given an array `arr` containing zeros and ones, this function expands the ones by replacing
-    the elements before and after each one with ones as well. The number of elements to replace
-    before and after each one is given by the parameters `N` and `M`, respectively.
-    
-    """
-    arr = np.array(arr)
+import moviepy.editor as mp
+from loguru import logger as log
 
-    # Find the indices of the zeros in the array
-    N = int(N)
-    M = int(M)
-    print(arr.sum() / arr.size)
-    zero_indices = np.where(arr == 1)[0]
-
-    # Replace the elements before and after each zero with zeros
-    for i in zero_indices:
-        start = max(0, i - N)
-        end = min(len(arr), i + M + 1)
-        arr[start:end] = 1
-    print(arr.sum() / arr.size)
-    return arr
-
-def get_subframe(image, position, subframe_size):
-    """
-    Given an input image and a position, extract a subframe of size subframe_size
-    """
-    rows, cols, _ = image.shape
-
-    frame_rows, frame_cols = subframe_size
-
-    row_min = position[0] - frame_rows // 2
-    col_min = position[1] - frame_cols // 2
-
-    row_min = max(row_min, 0)
-    col_min = max(col_min, 0)
-    if row_min + frame_rows > rows:
-        row_min = rows - frame_rows
-    if col_min + frame_cols > cols:
-        col_min = cols - frame_cols
-
-    return image[row_min:row_min+frame_rows, col_min:col_min+frame_cols]
-
-def create_subclip(clip, indices):
-    """
-    Given a clip and the indices of the relavant frames, create a clip only with those frames
-    """
-    subclips = []
-
-    start = 0
-    for i in range(1, len(indices)):
-        if indices[i] != indices[i-1] + 1:
-            # If the current index is not consecutive with the previous index, create a new subclip
-            subclips.append(clip.subclip(t_start=indices[start] / clip.fps, t_end=indices[i-1] / clip.fps))
-            start = i
-
-    subclips.append(clip.subclip(t_start=indices[start] / clip.fps, t_end=indices[-1] / clip.fps))
-    
-    subclip = mp.concatenate_videoclips(subclips)
-
-    return subclip
+from apex_highlight_creator import (create_highlight,
+                                    generate_inputs_from_image,
+                                    make_sure_folders_exist)
 
 if __name__ == '__main__':
+    # if True, generate inputs from the videos
+    # if False, use the inputs to predict
+    # you need to generate the inputs only once
+    # then sort them in the inputs folder
+    # then you run ml.py
+    # then you can use the model to predict
     generate_inputs = False
 
     # folder where the videos are
     folder = r"D:\Videos\Radeon ReLive\Apex Legends\a"
 
-    # if in input generation, generate one input every 
+    # if in input generation, generate one input every
     # this number needs to be high because we have to see a variaty of situations
     # many guns, many champions, many maps...
     input_generation_sampling = 50
@@ -88,63 +31,35 @@ if __name__ == '__main__':
     predict_sampling = 5
 
     # keep this number of seconds before and after an interesting frame
-    keep_after = 5 # sec
-    keep_before = 4 # sec
-    
-    if not generate_inputs:
-            # We need to load the model to make prediction if a frame is interesting or not
-            path = Path('inputs')
-            data = ImageDataLoaders.from_folder(path, train='train', valid='valid')
-            learn = vision_learner(data, models.resnet18, bn_final=True, model_dir="models")
-            learn = learn.load('resnet18')
+    keep_after = 5  # sec
+    keep_before = 4  # sec
 
-    for filename in os.listdir(folder):
-        if not filename.endswith(".mp4"):
-            continue
+    # size of the regions of interest
+    roi_size = (100, 100)
 
-        path = os.path.join(folder, filename)
-        print(f'Working on {path}.')
-        clip = mp.VideoFileClip(path)
+    # name and positions of the regions of interest
+    rois = {'center': (719, 1289), 'teamsleft': (88, 2049)}
 
-        times = np.arange(0, clip.duration, 1.0/clip.fps)
+    # default image size
+    default_image_size = (2560, 1440)
 
-        subframes = {}
-        for i, t in tqdm(enumerate(times), total = times.size):
-            if i % predict_sampling != 0:
-                continue
-            try:
-                frame = clip.get_frame(t)
-                subframes[i] = {'center': get_subframe(frame, (719, 1289), (100, 100)),
-                        'teamsleft': get_subframe(frame, (88, 2049), (100, 100))}
-            except Exception:
+    make_sure_folders_exist()
+
+    log.info(f'Working on {folder}.')
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        for filename in os.listdir(folder):
+            if not filename.endswith(".mp4"):
                 continue
 
-        mask = np.zeros(int(clip.duration * clip.fps))
-        for i, subf in tqdm(subframes.items()):
-            pred = []
-            for key, subframe in subf.items():
-                subframe = subframe.astype(np.uint8)
-                if not generate_inputs:
-                    with learn.no_bar():
-                        pred_class, pred_idx, outputs = learn.predict(subframe)
-                    pred.append(pred_class == 'true')
-                    if i%5 == 0:
-                        img_path = f"outputs/{pred_class}/{key}_{filename[-20:]}_{i/60:.2f}.png"
-                        if not os.path.exists(img_path):
-                            imageio.imwrite(img_path, subframe)
-                else:
-                    img_path = f"inputs/{key}_{filename[-20:]}_{i/60:.2f}.png"
-                    if not os.path.exists(img_path):
-                        imageio.imwrite(img_path, subframe)
+            log.info(f'Working on {filename}.')
 
-            mask[i] = any(pred)
+            path = os.path.join(folder, filename)
 
-        if not generate_inputs:
-            mask = expand_ones(mask, keep_before * clip.fps, keep_after * clip.fps)
+            clip = mp.VideoFileClip(path)
 
-            mask_indices = mask.nonzero()[0]
-
-            if len(mask_indices) > 0:
-                filtered_clip = create_subclip(clip, mask_indices)
-
-                filtered_clip.write_videofile(f"videos/{filename[:-4]}_shortened.mp4")
+            if generate_inputs:
+                generate_inputs_from_image(
+                    clip, filename, rois, roi_size, input_generation_sampling)
+            else:
+                executor.submit(create_highlight, clip, filename,
+                                predict_sampling, keep_before, keep_after, rois, roi_size)
