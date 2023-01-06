92085b438f3d6e9e9da71d6f19a87f59d3ea9912
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-06 21:22:43 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -4,42 +4,40 @@ from fastai.vision.learner import vision_learner
 from fastai.metrics import accuracy, RocAucBinary
 from fastai.vision.models import resnet18
 from fastai.interpret import Interpretation
-
+from loguru import logger as log
 
 if __name__ == '__main__':
+    path = Path('inputs')
     epochs = 30
 
-    path = Path('inputs')
-
     data = ImageDataLoaders.from_folder(path, train='train', valid='valid')
-    print(f'training set: {len(data.train_ds)}')
-    print(f'validation set: {len(data.valid_ds)}')
+    log.info(f'training set: {len(data.train_ds)}')
+    log.info(f'validation set: {len(data.valid_ds)}')
 
-    print("Creating Model.")
+    log.info("Creating Model.")
     input_shape = data.one_batch()[0].shape[1:]
     learn = vision_learner(data, resnet18, bn_final=True, model_dir="models", metrics=[accuracy,RocAucBinary()])
     
-    print("Loading weights.")
+    log.info("Loading weights.")
     learn.load('resnet18')
 
-    print("Printing summary.")
+    log.info("Creating model summary file.")
     with open('inputs/models/summary.txt', 'w') as f:
         f.write(learn.summary())
 
-    print("Printing results.")
-    interpretation = Interpretation.from_learner(learn)
-    interpretation.show_results([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    log.info("Showing results.")
+    #interpretation = Interpretation.from_learner(learn)
+    #interpretation.show_results([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-    print("Validating.")
-    learn.validate()
-    print(learn.validate())
+    log.info("Validating.")
+    log.info(learn.validate())
 
-    print("Printing confusion matrix.")
-    learn.show_results()
+    log.info("Printing confusion matrix.")
+    log.info(learn.show_results())
 
     for _ in range(epochs//5):
-        print("Training.")
+        log.info("Training.")
         learn.fit(5)
 
         learn.save('resnet18')
-        print(f'Epoch {learn.epoch} done.')
+        log.info(f'Epoch {learn.epoch} done.')
