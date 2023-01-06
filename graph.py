ac456f5bf9b15afb38ceabc39b223f170680f741
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-06 21:22:45 +0100

    Update graph.py

--- graph.py
+++ graph.py
@@ -6,7 +6,9 @@ from fastai.metrics import accuracy
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 from fastai.vision.models import resnet18
-from sklearn.metrics import roc_curve, auc
+from loguru import logger as log
+from sklearn.metrics import auc, roc_curve
+from tqdm import tqdm
 
 
 def plot_roc_curve(y_test, y, path):
@@ -33,19 +35,38 @@ def plot_n_images(images, pred, true, path):
     n_rows = int(np.sqrt(n_images))
     n_cols = int(np.sqrt(n_images))
 
-    fig, axs = plt.subplots(n_rows, n_cols, figsize=(10, 10))
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(1.5*n_rows, 1.5*n_cols))
 
     for i, ax in enumerate(axs.flat):
         if i > len(axs.flat):
             break
         ax.imshow(images[i])
-        ax.set_title(f'Pred:{pred[i]:.2f} GT:{true[i]:.2f}')
+        # set title to TP, FP, TN, FN
+        title = ''
+        if pred[i] > 0.5 and true[i] > 0.5:
+            title = 'TP'
+        elif pred[i] > 0.5 and true[i] < 0.5:
+            title = 'FP'
+        elif pred[i] < 0.5 and true[i] < 0.5:
+            title = 'TN'
+        elif pred[i] < 0.5 and true[i] > 0.5:
+            title = 'FN'
+            
+        ax.set_title(f'{title} Pred:{pred[i]:.2f}')
         ax.axis('off')
 
     fig.tight_layout()
     
     fig.savefig(path)
-    print(f'Saved {path}')
+    log.info(f'Saved {path}')
+
+def plot_n_images_by_idxs(images, pred, true, path, idxs):
+    sorted_images = [images[i] for i in idxs]
+    sorted_predictions = [pred[i] for i in idxs]
+    sorted_y_test = [true[i] for i in idxs]
+
+    plot_n_images(sorted_images, sorted_predictions, sorted_y_test, path)
+
 
 
 if __name__ == '__main__':
@@ -57,44 +78,36 @@ if __name__ == '__main__':
 
     save_path = Path('graphs')
     save_path.mkdir(exist_ok=True)
-    
-    print(f'Predicting {len(data.valid_ds)} images...')
+
+    log.info(f'Predicting {len(data.valid_ds)} images...')
     y_test = np.array([img[1].numpy() for img in data.valid_ds])
-    y = np.array([learn.predict(img[0])[2][1].numpy() for img in data.valid_ds])
-    print(y)
+    y = np.array([learn.predict(img[0])[2][1].numpy() for img in tqdm(data.valid_ds)])
     imgs = [np.asarray(img[0]) for img in data.valid_ds]
 
-    print('Plotting ROC curve...')    
+    sorted_idxs = np.argsort(y_test)
+    sorted_idxs = [e for i,e in enumerate(sorted_idxs) if i % 10 == 0]
+    sorted_idxs = np.concatenate([sorted_idxs[:50], sorted_idxs[-50:]])
+    plot_n_images_by_idxs(imgs, y, y_test, save_path / 'random_images.png', sorted_idxs)
+
     # Plot ROC curve
+    log.info('Plotting ROC curve...')
     plot_roc_curve(y_test, y, save_path / 'roc.png')
 
-    print('Plotting most uncertain images...')
     # Plot most uncertain images
+    log.info('Plotting most uncertain images...')
     certainties =  abs(0.5 - y)
-    sorted_idxs = np.argsort(certainties)[:25]
-    sorted_images = [imgs[i] for i in sorted_idxs]
-    sorted_predictions = [y[i] for i in sorted_idxs]
-    sorted_y_test = [y_test[i] for i in sorted_idxs]
-
-    plot_n_images(sorted_images, sorted_predictions, sorted_y_test, save_path / 'most_uncertain.png')
-
     sorted_idxs = np.argsort(certainties)
-    sorted_images = [imgs[i] for i in sorted_idxs]
-    sorted_predictions = [y[i] for i in sorted_idxs]
+
+    plot_n_images_by_idxs(imgs, y, y_test, save_path / 'most_uncertain.png', sorted_idxs[:25])
+
     sorted_y_test = [y_test[i] for i in sorted_idxs]
 
     # indexes of true values
     false_idxs = np.where(np.array(sorted_y_test) == 0)[0]
-    false_images = [sorted_images[i] for i in false_idxs][:25]
-    false_predictions = [sorted_predictions[i] for i in false_idxs][:25]
-    false_y_test = [sorted_y_test[i] for i in false_idxs][:25]
 
-    plot_n_images(false_images, false_predictions, false_y_test, save_path / 'most_uncertain_false.png')
+    plot_n_images_by_idxs(imgs, y, y_test, save_path / 'most_uncertain_false.png', false_idxs[:25])
 
     # indexes of true values
     true_idxs = np.where(np.array(sorted_y_test) == 1)[0]
-    true_images = [sorted_images[i] for i in true_idxs][:25]
-    true_predictions = [sorted_predictions[i] for i in true_idxs][:25]
-    true_y_test = [sorted_y_test[i] for i in true_idxs][:25]
 
-    plot_n_images(true_images, true_predictions, true_y_test, save_path / 'most_uncertain_true.png')
+    plot_n_images_by_idxs(imgs, y, y_test, save_path / 'most_uncertain_true.png', true_idxs[:25])
