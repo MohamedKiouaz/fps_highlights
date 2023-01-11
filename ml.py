7d0f79016597ed872178fea283145d5e45880e59
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-11 19:37:54 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+
+from fastai.metrics import RocAucBinary, accuracy
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
-from fastai.metrics import accuracy, RocAucBinary
 from fastai.vision.models import resnet18
-from fastai.interpret import Interpretation
+from fastai.vision.augment import Resize
 from loguru import logger as log
 
 def create_model():
@@ -12,6 +13,9 @@ def create_model():
     log.info("Loading data.")
     data = ImageDataLoaders.from_folder(path, train='train', valid='valid')
     
+    # resize to 100x100
+    data.add_tfms(Resize(100, method='squish'), 'before_batch')
+
     log.info("Creating Model.")
     model = vision_learner(data, resnet18, bn_final=True, model_dir="models", metrics=[accuracy,RocAucBinary()])
 
@@ -20,7 +24,6 @@ def create_model():
 
     log.info("Done.")
     return model, data
-
 
 if __name__ == '__main__':
     epochs = 30
