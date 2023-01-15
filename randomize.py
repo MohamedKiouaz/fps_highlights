36ab2a0ade8df9a0023cb308e419c4f451252492
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-15 17:54:13 +0100

    Update randomize.py

--- randomize.py
+++ randomize.py
@@ -1,6 +1,9 @@
 import glob
+import os
 import shutil
+
 import numpy as np
+from PIL import Image
 from tqdm import tqdm
 
 if __name__ == '__main__':
@@ -18,7 +21,7 @@ if __name__ == '__main__':
 
     train = np.random.choice(train, int(len(train) * ratio), replace=False)
     valid = np.random.choice(valid, int(len(valid) * (1 - ratio)), replace=False)
-    
+
     for path in tqdm(train):
         # move to valid
         new_path = path.replace('train', 'valid')
@@ -28,9 +31,23 @@ if __name__ == '__main__':
         # move to train
         new_path = path.replace('valid', 'train')
         shutil.move(path, new_path)              
-    
+
     print('Done.')
 
     shutil.rmtree('inputs/models', ignore_errors=True)
 
     print('Removed models.')
+
+    i = 0
+    images = glob.glob('inputs/**/*.png', recursive=True)
+    for image in tqdm(images):
+        try:
+            img = Image.open(image)
+            img.verify()
+        except Exception:
+            print(f'Failed to open {image}')
+            if i < 10:
+                #remove
+                os.remove(image)
+                i += 1
+                print(f'Removed {image}')
