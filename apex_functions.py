64b2347874fcaf3a57de370dd34796a6a5a5cba3
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-01-19 20:08:30 +0100

    Create apex_functions.py

---
+++ apex_functions.py
@@ -0,0 +1,8 @@
+import os 
+
+def create_folders():
+    """
+    Create the folders where we will store the inputs and outputs
+    """
+    for folder in ['inputs/models', 'inputs/train/true', 'inputs/train/false', 'inputs/valid/true', 'inputs/valid/false', 'outputs/true', 'outputs/false', 'videos']:
+        os.makedirs(folder, exist_ok=True)
