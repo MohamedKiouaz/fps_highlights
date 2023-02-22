1e0a7b4dd3577dedb73c4eaa39842ecaeaad14a0
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-02-22 21:45:14 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -4,21 +4,24 @@ from fastai.metrics import RocAucBinary, accuracy
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 from fastai.vision.models import resnet18
-from fastai.vision.augment import Resize
+from fastai.vision.augment import Resize, RandomErasing, RandomResizedCrop
 from loguru import logger as log
 
 from fps_functions import create_folders
 
 from tqdm import tqdm
 
-def create_model():
+def create_model(train=False):
     path = Path('inputs')
 
     log.info("Loading data.")
     data = ImageDataLoaders.from_folder(path, train='train', valid='valid') 
     
-    # resize to 100x100
     data.add_tfms(Resize((100, 100), method='squish'), 'after_item')
+    
+    if train:
+        data.add_tfms(RandomResizedCrop((100, 100), min_scale=0.95), 'after_item')
+        #data.add_tfms(RandomErasing(), 'after_item')
 
     log.info("Creating Model.")
     model = vision_learner(data, resnet18, bn_final=True, model_dir="models", metrics=[accuracy,RocAucBinary()])
@@ -35,7 +38,7 @@ if __name__ == '__main__':
 
     create_folders()
 
-    learn, data = create_model()
+    learn, data = create_model(train=True)
     
     log.info(f'Training set: {len(data.train_ds)}')
     log.info(f'Validation set: {len(data.valid_ds)}')
