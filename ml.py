9cba262f8fe022d72cfd667aa041586280b4408d
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-04-23 07:01:16 +0200

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 from fastai.metrics import RocAucBinary, accuracy
+from fastai.vision.augment import RandomErasing, RandomResizedCrop, Resize
 from fastai.vision.data import ImageDataLoaders
 from fastai.vision.learner import vision_learner
 from fastai.vision.models import resnet18
-from fastai.vision.augment import Resize, RandomErasing, RandomResizedCrop
 from loguru import logger as log
+from tqdm import tqdm
 
 from fps_functions import create_folders
 
-from tqdm import tqdm
 
 def create_model(train=False):
     path = Path('inputs')
@@ -47,10 +47,6 @@ if __name__ == '__main__':
     with open('inputs/models/summary.txt', 'w+') as f:
         f.write(learn.summary())
 
-    #log.info("Showing results.")
-    #interpretation = Interpretation.from_learner(learn)
-    #interpretation.show_results([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
     #log.info("Validating.")
     #log.info(learn.validate())
 
@@ -59,6 +55,7 @@ if __name__ == '__main__':
 
     for _ in range(epochs):
         log.info("Training.")
+        # dataset has 180k images, so 1 epoch is enough
         learn.fit(1)
         log.info(f'Epoch {learn.epoch} done.')
       
