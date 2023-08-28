055e0742bf671d142d8d078386019b6019523f76
MohamedKiouaz <mohamed.kiouaz@gmail.com>
2023-08-28 22:06:02 +0200

    Update fps_functions.py

--- fps_functions.py
+++ fps_functions.py
@@ -1,9 +1,21 @@
 import os
 
 
+def _class_folders(folder):
+    """
+    Returns the folders where we will store the images
+    """
+    return [os.path.join(folder, 'true'), os.path.join(folder, 'false'), os.path.join(folder, 'rank'), os.path.join(folder, 'shield')]
+
 def create_folders():
     """
     Create the folders where we will store the inputs and outputs
     """
-    for folder in ['inputs/models', 'inputs/train/true', 'inputs/train/false', 'inputs/valid/true', 'inputs/valid/false', 'outputs/true', 'outputs/false', 'videos', 'temp', 'low_confidence/true', 'low_confidence/false']:
+    folders = ['inputs/train', 'inputs/valid', 'outputs', 'low_confidence']
+    for folder in folders.copy():
+        folders += _class_folders(folder)
+
+    folders += ['inputs/models', 'videos', 'temp']
+    
+    for folder in folders:
         os.makedirs(folder, exist_ok=True)
